77070e568d9f1c53284f8ab61e116057c5981922
pipaelgenial <pedrompsantos84@gmail.com>
2026-06-29 23:10:18 +0100

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
+// LOGIN
 export const login = (email, password) =>
   api.post("/auth/login", { email, password });
 
+// REGISTER
 export const register = (name, email, password) =>
   api.post("/auth/register", { name, email, password });
 
+// GET CURRENT USER
 export const getMe = (token) =>
   api.get("/auth/me", {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 
 export default api;
